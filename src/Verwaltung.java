0d2444a26210466976f76ec20f02638f8463de22
besenlohntsich <bessim.hasani@gmail.com>
2024-11-19 13:11:03 +0100

    verwaltung import und erste überführung UML

---
+++ src/Verwaltung.java
@@ -0,0 +1,64 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Verwaltung {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vigenere vigenere = new Vigenere();
+        int option;
+
+        do {
+            System.out.println("Wähle eine Option:");
+            System.out.println("0 - Beenden");
+            System.out.println("1 - Verschlüsseln");
+            System.out.println("2 - Entschlüsseln");
+
+            //verhindert crash durch eingabe einen falschen datentyps
+            while (true) {
+                try {
+                    option = sc.nextInt();
+                    sc.nextLine();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Ungültige Eingabe. Bitte eine Zahl eingeben.");
+                    sc.next();
+                }
+            }
+
+
+            switch (option) {
+                case 0:
+                    //spiegelt den wert von option wider und vergleicht diesen
+                    System.out.println("Programm wird beendet.");
+                    break;
+                case 1:
+                    System.out.println("Gebe eine Nachricht zum Verschlüsseln ein:");
+                    vigenere.setKt(sc.nextLine());
+
+                    System.out.print("Gebe den Buchstaben Schlüssel ein:");
+                    vigenere.setS(sc.nextLine());
+                    sc.nextLine();
+
+                    vigenere.verschluesseln();
+                    System.out.println("Verschlüsselte Nachricht: " + vigenere.getGt());
+                    break;
+                case 2:
+                    System.out.println("Gebe die verschlüsselte Nachricht ein:");
+                    vigenere.setGt(sc.nextLine());
+
+                    System.out.print("Gebe den Buchstaben Schlüssel ein:");
+                    vigenere.setS(sc.nextLine());
+                    sc.nextLine();
+
+                    vigenere.entschluesseln();
+                    System.out.println("Entschlüsselte Nachricht: " + vigenere.getKt());
+                    break;
+                default:
+                    System.out.println("Ungültige Option, bitte wähle erneut.");
+            }
+        } while (option != 0);
+        //ruft alles unter dem do wieder auf, wenn option ungleich 0 ist
+        sc.close();
+    }
+}
---
+++ src/Vigenere.java
@@ -0,0 +1,55 @@
+public class Vigenere {
+
+
+    private String kt;
+
+    private String s;
+
+    private String gt;
+
+
+    public Vigenere() {
+
+        this.gt = "";
+        this.kt = "";
+        this.s = "";
+
+    }
+
+
+
+        public void verschluesseln () {
+
+
+        }
+
+        public String getGt ()
+        {
+            return gt;
+        }
+
+        public void setGt (String gt)
+        {
+            this.gt = gt;
+        }
+
+        public String getS ()
+        {
+            return s;
+        }
+
+        public void setS (String s)
+        {
+            this.s = s;
+        }
+
+        public String getKt ()
+        {
+            return kt;
+        }
+
+        public void setKt (String kt)
+        {
+            this.kt = kt;
+        }
+    }
