523e40d6ea12dc0f54ac9457b6085253e94dd04e
besenlohntsich <bessim.hasani@gmail.com>
2024-11-20 10:47:13 +0100

    verschlüsselungs impl

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -8,21 +8,60 @@ public class Vigenere {
     private String gt;
 
 
-    public Vigenere() {
+    public Vigenere()
+    {
 
         this.gt = "";
         this.kt = "";
-        this.s = "";
+        this.s ="";
 
     }
 
 
-
+         //nur großbuchstaben werden verschlüsselt
         public void verschluesseln () {
+            String result = "";
+
+
+
+            for (int i = 0; i < kt.length(); i++) {
+            //Wir gehen durch den Array
+                char Zs = (s.charAt(s.charAt(i)));
+                //Zs soll der Ascii wert des schlüssels sein
+
+                char ch = Character.toUpperCase(kt.charAt(i));
+                //buchstaben werden zuerst in Großbuchstaben verwandelt
+
+                if (Character.isUpperCase(ch)) {
+                    ch = zahlenZuBuchstaben((buchstabenZuZahlen(ch) - buchstabenZuZahlen(Zs) + 26) % 26);
+                }
+
+                result += ch;
+            }
+
+            gt = result;
+
+        }
+
+        public void entschluesseln()
+        {
 
 
         }
 
+
+
+        private int buchstabenZuZahlen(char pWert)
+        {
+
+        return pWert - 'A';
+        }
+
+        private char zahlenZuBuchstaben(int pWert)
+        {
+        return (char) (pWert + 'A');
+
+        }
         public String getGt ()
         {
             return gt;
